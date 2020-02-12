8222c150034ce99c77e51060b07cc60fc908c3b4
Brett Bedevian <brettbedevian@gmail.com>
2020-02-12 17:26:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = {:railroads => {:pieces => 4, :rent_in_dollars => {}}}
+  monopoly = {:railroads => {:pieces => 4, :rent_in_dollars => {:key1 ,:key2 , :key3 , :key4 }
+  }}}
 end
 
 def monopoly_with_fourth_tier
